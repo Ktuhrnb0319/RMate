0c68dbd15d00aeb27fbc5abec08d6ca2135de4b2
Ktuhrnb0319 <ktuhrnb@gmail.com>
2021-05-24 14:11:41 +0900

    メニューモデル単体テストコード

---
+++ spec/factories/menus.rb
@@ -0,0 +1,11 @@
+FactoryBot.define do
+  factory :menu do
+    training_date {'2021-05-23'}
+    part {"Chest"}
+    training {"ベンチプレス"}
+    weight {100}
+    rep {10}
+    rm {120}
+    association :user 
+  end
+end
---
+++ spec/models/menu_spec.rb
@@ -0,0 +1,69 @@
+require 'rails_helper'
+
+RSpec.describe Menu, type: :model do
+  before do
+    @menu = FactoryBot.build(:menu)
+  end
+
+  describe 'トレーニング記録機能' do
+
+
+    context 'トレーニングを記録できるとき' do
+
+      it 'すべての値が正しく入力されていれば保存できること' do
+        expect(@menu).to be_valid
+      end
+
+      it "weightは空でも保存できること" do
+        @menu.weight=""
+        expect(@menu).to be_valid
+      end
+
+      it "rmは空でも保存できること" do
+        @menu.rm=""
+        expect(@menu).to be_valid
+      end
+
+      context 'トレーニングを記録できないとき' do
+
+        it 'training-dateが空だと保存できない' do
+          @menu.training_date=""
+          @menu.valid?
+          expect(@menu.errors.full_messages).to include("Training date can't be blank")
+        end
+        
+        it 'partが空だと保存できない' do
+          @menu.part=""
+          @menu.valid?
+          expect(@menu.errors.full_messages).to include("Part can't be blank")
+        end
+
+        it 'trainingが空だと保存できない' do
+          @menu.training=""
+          @menu.valid?
+          expect(@menu.errors.full_messages).to include("Training can't be blank")
+        end
+
+        it 'repが空だと保存できない' do
+          @menu.rep=""
+          @menu.valid?
+          expect(@menu.errors.full_messages).to include("Rep can't be blank")
+        end
+
+        
+
+        
+
+
+      end
+
+
+
+
+    end
+
+
+
+  end
+
+end
